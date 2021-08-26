df80c1d017b8b1eb772d09ebfe57f206dc7e8a66
Sofiia Paniushkina <71231699+Sofa-Vicario@users.noreply.github.com>
2021-08-26 12:54:58 +0300

    newmain.cpp

---
+++ newmain.cpp
@@ -0,0 +1,17 @@
+#include "physical_analysis/Flow/real_flow/MpdCalculator.h"
+ #include "physical_analysis/Flow/real_flow/MpdCalculator.cxx"
+   // #include "physical_analysis/Flow/create_reduced_tree/reducedTreeCreator.C"
+
+   int main()
+   {
+    TString inFileName, outFileName, dcaFileName;
+    // inFileName = "/home/ubuntu/rootcern/AuAu_ecm7.7GeV_hydroON_EoS1PT_0-16fm_500ev_1000-1.reco.root";
+    MpdCalculator mpd = MpdCalculator(inFileName, outFileName, dcaFileName);                                                  //
+    mpd.CalculateResolutions(0);                                                                                                //
+    mpd.Write();
+
+    // reducedTreeCreator rtc = reducedTreeCreator("inFileHistName", "inFileTreeName", "outFileName", "dcaFileName");   //
+    // rtc.CreateReducedTree();
+
+    return 0;
+   }
